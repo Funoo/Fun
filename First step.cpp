41d5ef00a8ae77cb98a3a15dcf6978cbc44db043
昱帆 <42029541+Funoo@users.noreply.github.com>
2018-08-13 20:51:58 +0800

    exercise

---
+++ First step.cpp
@@ -0,0 +1,12 @@
+// myfirst.cpp -- display a message
+
+#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "²©ÑóÊÇ¸öÉµ×Ó.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+	return 0; 
+}
+
